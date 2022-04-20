8687bd4f79d1af65c9b2f5a4befd93a5995a8105
Jonathan Lavén <jonathan.laven123@gmail.com>
2022-04-20 15:38:51 +0200

    Co-authored-by: All3eee <All3eee@users.noreply.github.com> Co-authored-by: WilliamSalenius <WilliamSalenius@users.noreply.github.com> Co-authored-by: jompis <jompis@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+var card = {
+}
+for (let i = 2; i <= 10; i++){
+    card[`${i}_of_clubs`] = i;
+    card[`${i}_of_diamonds`] = i;
+    card[`${i}_of_diamonds`] = i;
+    card[`${i}_of_spades`] = i;
+    
+}
+console.log(card.klöver10)
