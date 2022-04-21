ea23e22d48d5b28ab3c6b1b1927b0092503c8af0
William Salenius <william.salenius@outlook.com>
2022-04-21 10:49:19 +0200

    bruh

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 var card = {
 }
-for (let i = 2; i <= 10; i++){
-    card[`${i}_of_clubs`] = i;
-    card[`${i}_of_diamonds`] = i;
-    card[`${i}_of_diamonds`] = i;
-    card[`${i}_of_spades`] = i;
+for (let i = 2; i <= 14; i++){
+    if (10 < i < 14) {
+        cardvalue = 10;
+    } else if ( i == 14) {
+        cardvalue = 11;
+    } else {
+        cardvalue = i;
+    }
+    card[`${i}_of_clubs`] = cardvalue;
+    card[`${i}_of_diamonds`] = cardvalue;
+    card[`${i}_of_diamonds`] = cardvalue;
+    card[`${i}_of_spades`] = cardvalue;
     
 }
 console.log(card.klöver10)
