3c044345e3f9dae0a029d7d1f7f3aa012eda9627
All3eee <alvin.havren@skola.taby.se>
2022-04-28 08:46:44 +0200

    file update

---
+++ knappar.js
@@ -0,0 +1,8 @@
+let Button1image = document.getElementById("button-1-container");
+
+let btn1 = document.getElementById("button-1-container");
+btn1.addEventListener("click", test)
+
+function test() {
+    console.log("William är fet")
+}
