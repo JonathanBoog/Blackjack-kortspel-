e502096635ab4860785677912edff8bdaf785e4f
Jonathan Lavén <jonathan.laven123@gmail.com>
2022-05-17 21:38:36 +0200

    deleted

--- knappar.js
+++
@@ -1,8 +0,0 @@
-let Button1image = document.getElementById("button-1-container");
-
-let btn1 = document.getElementById("button-1-container");
-btn1.addEventListener("click", test)
-
-function test() {
-    console.log("William är fet")
-}
